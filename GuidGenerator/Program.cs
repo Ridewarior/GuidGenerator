d396d505a9179c759e5008fd94d17cb02930be7e
Antonio Magallon <antoniojmagallon@gmail.com>
2023-12-24 00:49:09 -0500

    Add project files.

---
+++ GuidGenerator/Program.cs
@@ -0,0 +1,9 @@
+﻿using TextCopy;
+
+var guid = Guid.NewGuid().ToString();
+ClipboardService.SetText(guid);
+
+Console.Write("GUID copied to clipboard: ");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine(guid);
+Console.ResetColor();
